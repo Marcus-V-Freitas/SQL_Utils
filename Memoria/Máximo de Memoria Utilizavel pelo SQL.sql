64b5c62ce9788a4fdc0f91caeb53bfcdf237ff74
Marcus Vinicius de Freitas Costa <53195917+Marcus-V-Freitas@users.noreply.github.com>
2021-08-11 21:59:26 -0300

    Adicionada pasta com arquivos de memoria

---
+++ Memoria/Máximo de Memoria Utilizavel pelo SQL.sql
@@ -0,0 +1,4 @@
+select counter_name ,cntr_value,cast((cntr_value/1024.0)/1024.0 as numeric(8,2)) as Gb
+from sys.dm_os_performance_counters
+where counter_name like '%Target server_memory%'
+OR counter_name like '%Total server_memory%';
